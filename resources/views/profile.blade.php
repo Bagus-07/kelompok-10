e4cc94d1cbfd0c023fd6c2574ed67b7b97c73fbb
swimmingghostduck-wq <swimmingghostduck@gmail.com>
2026-04-15 17:42:39 +0700

    profile placeholder

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profile - StayEase</title>
+</head>
+<body>
+
+<h1>My Profile</h1>
+
+<p>Name: John Doe</p>
+<p>Email: johndoe@gmail.com</p>
+<p>Phone: 08123456789</p>
+
+<a href="/home">Back to Home</a>
+
+</body>
+</html>
