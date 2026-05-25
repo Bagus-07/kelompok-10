ba2757af108deb21dc11d8a549b1ebd914ba48c8
BAGUS_SATRIO <bagussatrio1102@gmail.com>
2026-05-25 22:37:58 +0700

    perbaikan review migrati dan update auth pada admin

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -40,6 +40,7 @@ class LoginController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'user',
         ]);
 
         Auth::login($user);
