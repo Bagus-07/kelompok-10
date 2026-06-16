44e116ddc676700f0812e013467ef8334632cf39
BAGUS_SATRIO <bagussatrio1102@gmail.com>
2026-06-16 15:16:27 +0700

    crud user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,17 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::where('role', ['user','tamu'])->get();
+        $users = User::where('role', 'user')->get();
 
         return view(
             'pages.user',
             compact('users')
         );
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'      => 'required',
+            'email'     => 'required|email|unique:users',
+            'phone'     => 'nullable',
+            'password'  => 'required|min:6'
+        ]);
+
+        User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'phone'     => $request->phone,
+            'password'  => Hash::make($request->password),
+            'role'      => 'user'
+        ]);
+
+        return redirect()->back()
+            ->with('success','Tamu berhasil ditambahkan');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $request->validate([
+            'name'  => 'required',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'phone' => 'nullable'
+        ]);
+
+        $user->update([
+            'name'  => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone
+        ]);
+
+        return redirect()->back()
+            ->with('success', 'User berhasil diupdate');
     }
 
     public function destroy($id)
